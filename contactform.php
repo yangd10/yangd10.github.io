3f6715c42a6e306b6aacc57b3f4b6155361fe75c
yangd10 <86544116+yangd10@users.noreply.github.com>
2021-08-12 20:01:25 -0400

    Create contactform.php

---
+++ contactform.php
@@ -0,0 +1,15 @@
+<?php
+
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $subject = $_POST['subject'];
+    $mailFrom = $_POST['mail'];
+    $message = $_POST['message'];
+
+    $mailTo = "davidmyang@berkeley.edu";
+    $headers = "From: ".$mailFrom;
+    $txt = "You have received an email from ".$name.".\n\n".$message;
+
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: index.php?mailsend");
+}
